bd17122100b0cb2fe0544716848c1a231bce943b
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-07-18 23:52:37 +0530

    done

---
+++ jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.sign-up').hide();
+  
+       
+    $('.btn-1').on('click',function(){
+    $('.sign-in').fadeOut(150);    
+    $('.sign-up').fadeIn(150);    
+    $('.form-main-banner').addClass('.sign-up');    
+    });
+  
+    $('.btn-2').on('click',function(){
+      $('.sign-up').fadeOut(150);    
+    $('.sign-in').fadeIn(150); 
+      $('.form-main-banner').removeClass('.sign-up');     
+    });
+  })
